dbd2804a05178fbc67a7ad34901e60009a23157f
이정구/책임연구원/Smart Box개발팀(junggu.lee@lge.com) <junggu.lee@lge.com>
2017-01-18 10:55:55 +0900

    테이블 삭제 후 생성 및 삽입 초기 버전 완성

--- tv_provider_ex.py
+++ tv_provider_ex.py
@@ -1,11 +1,76 @@
-# table 생성
+import sqlite3
+import timeit
+import csv
+import os
+#import pytest
 
-# records 삭제
+conn = sqlite3.connect('D:/github/tv_provider_ex/work_tv.db')
+cursor = conn.cursor()
 
-# records 삽입
+cursor.execute('''
+    DROP TABLE channels;
+    ''')
+conn.commit()
 
-# records 갱신 (비교없이)
+# create table
+cursor.execute('''
+    CREATE TABLE channels (
+      _id INTEGER PRIMARY KEY AUTOINCREMENT,
+      package_name TEXT NOT NULL,
+      input_id TEXT NOT NULL,
+      type TEXT NOT NULL DEFAULT 'TYPE_OTHER',
+      service_type TEXT NOT NULL DEFAULT 'SERVICE_TYPE_AUDIO_VIDEO',
+      original_network_id INTEGER NOT NULL DEFAULT 0,
+      transport_stream_id INTEGER NOT NULL DEFAULT 0,
+      service_id INTEGER NOT NULL DEFAULT 0,
+      display_number TEXT,
+      display_name TEXT,
+      network_affiliation TEXT,
+      description TEXT,
+      video_format TEXT,
+      browsable INTEGER NOT NULL DEFAULT 0,
+      searchable INTEGER NOT NULL DEFAULT 1,
+      selectable INTEGER,
+      locked INTEGER NOT NULL DEFAULT 0,
+      app_link_icon_uri TEXT,
+      app_link_poster_art_uri TEXT,
+      app_link_text TEXT,
+      app_link_color INTEGER,
+      app_link_intent_uri TEXT,
+      internal_provider_data BLOB,
+      internal_provider_flag1 INTEGER,
+      internal_provider_flag2 INTEGER,
+      internal_provider_flag3 INTEGER,
+      internal_provider_flag4 INTEGER,
+      logo BLOB,
+      version_number INTEGER,
+      channel_genre TEXT,cam_channel INTEGER,
+      UNIQUE(_id,package_name));
+        ''')
+conn.commit()
 
-# records 갱신 (비교하여)
+# read dummy
+matrix = []
+f = open('D:/github/tv_provider_ex/tv_db.csv', 'r')
+csvReader = csv.reader(f)
+for row in csvReader:
+    matrix.append(row)
 
-# unittest
+# insert data
+for row in matrix[1:]:
+    cursor.execute('''
+        INSERT INTO channels ('package_name', 'input_id', 'type',
+            'service_type', 'original_network_id', 'transport_stream_id',
+            'service_id', 'display_number', 'display_name')
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+        ''', (row[1], row[2], row[3], row[4], row[5],
+        row[6], row[7], row[8], row[9]))
+conn.commit()
+
+# display records
+cursor.execute('''
+    SELECT * FROM channels;
+    ''')
+print(cursor.fetchall())
+
+conn.close()
